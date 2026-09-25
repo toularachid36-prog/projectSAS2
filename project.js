39c9421c5b24510b07d4b42df5e8dcccaf464bff
toularachid36-prog <toularachid36@gmail.com>
2026-09-25 10:19:52 +0100

    4commit

--- project.js
+++ project.js
@@ -1,9 +1,9 @@
 const candidats = [{
 	cin : "AB123456",nom : "Boushaba", prenom : "Soufiane", partiPolitique : "Indépendant", age: 40, electeurs: []}
 ];
-const newcandidats = [
-	{cin :"PA183456", nom :"Ohssaine", prenom :"Abde Errahmane", partiPolitique :"Indépendant", age : 26, electeurs: []},
-    {cin :"MA343678", nom :"El Bennar", prenom :"mohammed",partiPolitique :"Indépendant", age : 27, electeurs: []}
-]
-candidats.push(...newcandidats);
-console.log(candidats);
+function ajouterplusfois (cin, nom, prenom, partiPolitique, age, electeurs){
+candidats.push({cin :"PA183456", nom :"Ohssaine", prenom :"Abde Errahmane", partiPolitique :"Indépendant", age : 26, electeurs: []},
+    {cin :"MA343678", nom :"El Bennar", prenom :"mohammed",partiPolitique :"Indépendant", age : 27, electeurs: []});
+}
+ajouterplusfois();
+	console.log(candidats);
