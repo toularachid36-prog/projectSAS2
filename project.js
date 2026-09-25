38d55e3c98513e686c413dc825e91fad873e3c2e
toularachid36-prog <toularachid36@gmail.com>
2026-09-25 09:49:44 +0100

    2commit

--- project.js
+++ project.js
@@ -1,8 +1,5 @@
 const candidats = [{
-	cin : "AB123456",
-	nom : "Boushaba",
-	prenom : "Soufiane",
-	partiPolitique : "Indépendant",
-	age: 40,
-	electeurs: []
-}];
+	cin : "AB123456",nom : "Boushaba", prenom : "Soufiane", partiPolitique : "Indépendant", age: 40, electeurs: []}
+];
+candidats.push({cin :"PA183456}", nom :"mohammed", partiPolitique :"Indépendant", age : 30, electeurs: []});
+console.log(candidats);
