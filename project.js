3e47e6eba5ef7b708c1450b9983e566a698b732d
toularachid36-prog <toularachid36@gmail.com>
2026-09-25 16:31:36 +0100

    6commit

--- project.js
+++ project.js
@@ -1,16 +1,101 @@
+let prompt = require('prompt-sync')();
 const candidats = [{
-	cin: "AB123456", nom: "Boushaba", prenom: "Soufiane", partiPolitique: "Indépendant", age: 40, electeurs: []
+	cin: "AB123456", nom: "Boushaba", prenom: "Soufiane", partiPolitique: "Indépendant", age: 40, electeurs: ["PA261531"]
 }
 ];
-function ajouternouveau(cin, nom, prenom, partiPolitique, age, electeurs) {
-	candidats.push({
-		cin: "MA098672", nom: "Ougali", prenom: "Hamza", partiPolitique: "Indépendant", age: 30, electeurs: []
-	});
+function ajouterCandidat(){
+	console.log("Ajouter un Candidat :");
+   let cin;
+    while (true) {
+        cin = prompt("Entrez Votre CIN :");
+        const candidatExiste = candidats.find(
+            candidat => candidat.cin === cin );
+        if (candidatExiste) {
+            console.log("Erreur : cette CIN existe déjà.");
+            console.log("Veuillez entrer une autre CIN.");
+        } else {
+            break;}}
+	let nom = prompt("Entrez Votre Nom :");
+	let prenom = prompt("Entrez Votre Prenom :");
+	let partiPolitique = prompt("Parti Politique OU Indépendant :");
+	let age = +prompt("Entrez Votre Age :");
+	const nouveaucandidat ={
+    cin : cin, nom : nom, prenom: prenom, partiPolitique: partiPolitique, age : age, electeurs : []}
+	candidats.push(nouveaucandidat);
+	console.log("Candidat Ajouter Avec Succés");
 }
-function ajouterplusfois(cin, nom, prenom, partiPolitique, age, electeurs) {
-	candidats.push({ cin: "PA183456", nom: "Ohssaine", prenom: "Abde Errahmane", partiPolitique: "Indépendant", age: 26, electeurs: [] },
-		{ cin: "MA343678", nom: "El Bennar", prenom: "mohammed", partiPolitique: "Indépendant", age: 27, electeurs: [] });
+function ajouterplufois(){
+console.log("Ajouter Plusieur Candidats :");
+const nombre = +prompt("Combien De Candidats Que Vous Voulez Ajouter :");
+if (isNaN(nombre) || nombre<=0){
+	console.log("Erreur : Nombre Invalide");
+	return;}
+for(let i = 0; i<nombre; i++){
+	console.log("____candidat" + (i + 1) + "____");
+    const cin = prompt("CIN :");
+	const candidatExiste = candidats.find(
+		candidat=>candidat.cin === cin);
+		if (candidatExiste){console.log("Erreur : c'est cin existe déja.");
+			i--;
+			continue;}
+	let nom = prompt("Entrez Votre Nom :");
+	let prenom = prompt("Entrez Votre Prenom :");
+	let partiPolitique = prompt("Parti Politique OU Indépendant :");
+	let age = +prompt("Entrez Votre Age :");
+	candidats.push({cin : cin, nom : nom, prenom: prenom, partiPolitique: partiPolitique, age : age, electeurs : []});
+	console.log("Candidats Ajouter Avec Succés");
 }
-ajouternouveau();
-ajouterplusfois();
-console.log(candidats);
+}
+function afficherliste(){
+	console.log("____Liste Des Candidats____");
+	if (candidats.length===0){
+		console.log("Aucun Candidat");
+		return;}
+	candidats.forEach((candidat, index)=>{
+		console.log("----Candidat"+ index + 1 + "---- :");
+		console.log("CIN :" + candidat.cin);
+		console.log("NOM :" + candidat.nom);
+		console.log("PRENOM :" + candidat.prenom);
+		console.log("Parti Politique :" + candidat.partiPolitique);
+		console.log("AGE :" + candidat.age);
+		console.log("Nombre De Votes :" +candidat.electeurs.length);
+	})
+}
+function afficherclassement(){
+        console.log("---Classement Par Nombre Des Votes---");
+		const Classement = [...candidats];
+	    Classement.sort(function(a, b)
+	    {return b.electeurs.length - a.electeurs.length;
+        });
+Classement.forEach(function(candidat, index) 
+{ console.log("--- Candidat " + (index + 1) + " ---"); 
+	console.log("CIN : " + candidat.cin); 
+	console.log("NOM : " + candidat.nom); 
+	console.log("PRENOM : " + candidat.prenom); 
+	console.log("Parti Politique : " + candidat.partiPolitique); 
+	console.log("AGE : " + candidat.age);
+	console.log("Nombre De Votes : " + candidat.electeurs.length); }); 
+}
+function filtrercandidat (){
+	console.log("---Filtrer Par Parti Politique---");
+	const parti = prompt("Entrez Le Parti Politique :");
+	const resultat = candidats.filter(function(candidat){
+    return candidat.partiPolitique.toLowerCase() === parti.toLowerCase()});
+	if(resultat.length === 0){
+			console.log("Aucun Candidat Trouve Pour Ce Parti");
+			return;}
+	console.log("----Candidat De Parti :" + parti);
+	resultat.forEach((candidat, index)=>{
+	console.log("--- Candidat " + (index + 1) + " ---"); 
+	console.log("CIN : " + candidat.cin); 
+	console.log("NOM : " + candidat.nom); 
+	console.log("PRENOM : " + candidat.prenom); 
+	console.log("Parti Politique : " + candidat.partiPolitique); 
+	console.log("AGE : " + candidat.age);
+	console.log("Nombre De Votes : " + candidat.electeurs.length); }); 	
+}
+ajouterCandidat();
+ajouterplufois();
+afficherliste();
+afficherclassement();
+filtrercandidat();
