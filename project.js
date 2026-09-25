845b02cbe3da9c3d8ce39215e0d953960883aa92
toularachid36-prog <toularachid36@gmail.com>
2026-09-25 10:26:52 +0100

    5commit

--- project.js
+++ project.js
@@ -1,9 +1,16 @@
 const candidats = [{
-	cin : "AB123456",nom : "Boushaba", prenom : "Soufiane", partiPolitique : "Indépendant", age: 40, electeurs: []}
-];
-function ajouterplusfois (cin, nom, prenom, partiPolitique, age, electeurs){
-candidats.push({cin :"PA183456", nom :"Ohssaine", prenom :"Abde Errahmane", partiPolitique :"Indépendant", age : 26, electeurs: []},
-    {cin :"MA343678", nom :"El Bennar", prenom :"mohammed",partiPolitique :"Indépendant", age : 27, electeurs: []});
+	cin: "AB123456", nom: "Boushaba", prenom: "Soufiane", partiPolitique: "Indépendant", age: 40, electeurs: []
 }
+];
+function ajouternouveau(cin, nom, prenom, partiPolitique, age, electeurs) {
+	candidats.push({
+		cin: "MA098672", nom: "Ougali", prenom: "Hamza", partiPolitique: "Indépendant", age: 30, electeurs: []
+	});
+}
+function ajouterplusfois(cin, nom, prenom, partiPolitique, age, electeurs) {
+	candidats.push({ cin: "PA183456", nom: "Ohssaine", prenom: "Abde Errahmane", partiPolitique: "Indépendant", age: 26, electeurs: [] },
+		{ cin: "MA343678", nom: "El Bennar", prenom: "mohammed", partiPolitique: "Indépendant", age: 27, electeurs: [] });
+}
+ajouternouveau();
 ajouterplusfois();
-	console.log(candidats);
+console.log(candidats);
